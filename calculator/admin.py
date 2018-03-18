5190feac88d611a55c9c0cec9bbd764c53f2e0bf
shilpi <shilpibiswas18@gmail.com>
2018-03-18 18:26:55 +0530

    databases partially updated

---
+++ calculator/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import ProductCatagory, OriginCity, Portcity, DestinationCity
+
+admin.site.register(ProductCatagory)
+admin.site.register(OriginCity)
+admin.site.register(Portcity)
+admin.site.register(DestinationCity)
+
+
---
+++ calculator/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+class ProductCatagory(models.Model):
+	product_type = models.CharField(max_length=50)
+	is_bulk = models.BooleanField(default=False)
+	gst_rate = models.DecimalField(max_digits=4, decimal_places=2)
+	custom_rate = models.DecimalField(max_digits=4, decimal_places=2)
+	insurance = models.DecimalField(max_digits=4, decimal_places=2)
+	transportable_by_sea = models.BooleanField(default=True)
+	transportable_by_road = models.BooleanField(default=True)
+	transportable_by_air = models.BooleanField(default=True)
+	transportable_by_rail = models.BooleanField(default=True)
+
+	def __str__(self):
+		return self.product_type
+
+	def __init__(self, arg):
+		super(ProductCatagory, self).__init__()
+		self.arg = arg
+
+
+class OriginCity(models.Model):
+	name = models.CharField(max_length=100)
+	latitude = models.DecimalField(max_digits=7, decimal_places=4)
+	longitude = models.DecimalField(max_digits=7, decimal_places=4)
+
+	def __str__(self):
+		return self.name
+
+	def __init__(self, arg):
+		super(OriginCity, self).__init__()
+		self.arg = arg
+
+
+class Portcity(models.Model):
+	name = models.CharField(max_length=100)
+	latitude = models.DecimalField(max_digits=7, decimal_places=4)
+	longitude = models.DecimalField(max_digits=7, decimal_places=4)
+	is_sea_port = models.BooleanField(default=True)
+	# handling_fare = models.Decimalfield(decimal_places=2)
+
+	def __str__(self):
+		return self.name
+
+	def __init__(self, arg):
+		super(Portcity, self).__init__()
+		self.arg = arg
+
+
+class DestinationCity(models.Model):
+	name = models.CharField(max_length=100)
+	latitude = models.DecimalField(max_digits=7, decimal_places=4)
+	longitude = models.DecimalField(max_digits=7, decimal_places=4)
+
+	def __str__(self):
+		return self.name
+	
+
+
